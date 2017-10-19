ee8673aff73fc6046edc3bb50866442b4cafa70e
Jesse B. Hannah <jesse@jbhannah.net>
2017-10-18 21:00:40 -0700

    Sort players by qualifying on frontend

--- src/components/Frontend.vue
+++ src/components/Frontend.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="frontend">
     <table>
-      <Player v-for="player in players" :key="player['.key']" :player="player" />
+      <Player v-for="player in sortedPlayers" :key="player['.key']" :player="player" />
     </table>
   </div>
 </template>
@@ -15,6 +15,13 @@ export default {
     players: {
       type: Array,
       required: true
+    }
+  },
+  computed: {
+    sortedPlayers: function () {
+      return this.players
+        .filter((p) => (p.laps || []).length > 0)
+        .sort((a, b) => a.laps[0] - b.laps[0])
     }
   },
   components: { Player }
